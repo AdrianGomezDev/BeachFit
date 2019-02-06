4c81ca7cf8768dd7680973fb3585505a387eccbc
AdrianGomezDev <43425907+AdrianGomezDev@users.noreply.github.com>
2019-02-06 14:53:28 -0800

    Initial commit

---
+++ app/src/main/java/com/example/beachfitlogin/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.beachfitlogin;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.beachfitlogin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView userText = (TextView) findViewById(R.id.textView);
+        //EditText userEdit = (EditText) findViewById(R.id.editText2);
+        String content = userText.getText().toString();
+        userText.setText(content);
+
+        userText.setTextColor(Color.BLACK);
+
+        TextView passText = (TextView) findViewById(R.id.textView2);
+
+
+
+        ImageView imgtest = (ImageView) findViewById(R.id.imageView);
+        imgtest.setImageResource(R.drawable.beachfitlogo);
+
+        EditText newUserText = (EditText) findViewById(R.id.editText);
+        newUserText.setKeyListener(null);
+
+        newUserText.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, NewAccount.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/beachfitlogin/NewAccount.java
@@ -0,0 +1,19 @@
+package com.example.beachfitlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.beachfitlogin.R;
+
+public class NewAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+
+        //ImageView imgtest = (ImageView) findViewById(R.id.imageView2);
+        //imgtest.setImageResource(R.drawable.male07);
+    }
+}
