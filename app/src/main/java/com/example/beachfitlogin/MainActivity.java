e84c10fd6fa37392f620b0687942d17f57b55c51
AdrianGomezDev <43425907+AdrianGomezDev@users.noreply.github.com>
2019-02-11 16:28:43 -0800

    I did it!

--- app/src/main/java/com/example/beachfitlogin/MainActivity.java
+++ app/src/main/java/com/example/beachfitlogin/MainActivity.java
@@ -5,40 +5,83 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.beachfitlogin.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    int loginAttempts = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView userText = (TextView) findViewById(R.id.usernameEditText);
-        //EditText userEdit = (EditText) findViewById(R.id.editText2);
-        String content = userText.getText().toString();
-        userText.setText(content);
-
-        userText.setTextColor(Color.BLACK);
-
-        TextView passText = (TextView) findViewById(R.id.passEditText);
+        // EditText for username
+        final EditText userText = (EditText) findViewById(R.id.usernameEditText);
+        //final String usernameContent = userText.getText().toString();
+        //userText.setText(usernameContent);
 
 
+        // EditText for password
+        final EditText passText = (EditText) findViewById(R.id.passEditText);
+        //final String passContent = passText.getText().toString();
+        //passText.setText(passContent);
 
-        ImageView imgtest = (ImageView) findViewById(R.id.imageView);
-        imgtest.setImageResource(R.drawable.beachfitlogo);
 
-        EditText newUserText = (EditText) findViewById(R.id.editText);
-        newUserText.setKeyListener(null);
+        // ImageView for beachfit logo
+        ImageView logoImg = (ImageView) findViewById(R.id.logoView);
+        logoImg.setImageResource(R.drawable.beachfitlogo);
 
-        newUserText.setOnClickListener(new View.OnClickListener(){
+        // Button for login
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setKeyListener(null);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+
+                // finish();
+                // System.exit(0);
+
+
+
+
+                if((userText.getText().toString().equals("admin")) && (passText.getText().toString().equals("1234"))){
+                    Toast.makeText(getApplicationContext(), "Login is successful!",
+                            Toast.LENGTH_LONG).show();
+                    // resets login attempts on successful login
+                    loginAttempts=0;
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Username or password is incorrect.",
+                            Toast.LENGTH_LONG).show();
+                    // increment login attempts counter
+                    loginAttempts++;
+                    // check for login attempts. if third unsuccesful login then exits the program.
+                    if(loginAttempts == 3){
+                        finish();
+                        System.exit(0);
+                    }
+                }
+
+            }
+        });
+
+
+        // Button for new user
+        Button newUserButton = (Button) findViewById(R.id.newUserButton);
+        newUserButton.setKeyListener(null);
+
+        newUserButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, NewAccount.class));
             }
+
         });
     }
 }
--- app/src/main/java/com/example/beachfitlogin/NewAccount.java
+++ app/src/main/java/com/example/beachfitlogin/NewAccount.java
@@ -2,18 +2,80 @@ package com.example.beachfitlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.beachfitlogin.R;
 
 public class NewAccount extends AppCompatActivity {
+
+    private boolean isEmpty(EditText eText){
+        if(eText.getText().toString().trim().length() > 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
 
-        //ImageView imgtest = (ImageView) findViewById(R.id.imageView2);
-        //imgtest.setImageResource(R.drawable.male07);
+        final EditText userText = (EditText) findViewById(R.id.userText);
+        final EditText passText = (EditText) findViewById(R.id.passText);
+        final EditText repassText = (EditText) findViewById(R.id.repassText);
+        final EditText firstText = (EditText) findViewById(R.id.firstText);
+        final EditText lastText = (EditText) findViewById(R.id.lastText);
+        final EditText emailText = (EditText) findViewById(R.id.emailText);
+        final EditText ageText = (EditText) findViewById(R.id.ageText);
+
+        //button for saving
+        // TEMPORARILY FOR CHECKING NEW ACCOUNT FIELDS
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setKeyListener(null);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                // checks if all fields in the new user activity page are empty
+                if(isEmpty(userText) || isEmpty(passText)|| isEmpty(repassText)|| isEmpty(firstText)|| isEmpty(lastText)|| isEmpty(emailText)|| isEmpty(ageText)){
+                    Toast.makeText(getApplicationContext(), "Please enter all required fields.",
+                            Toast.LENGTH_LONG).show();
+                }
+
+
+                // checks if the password and retype password fields match
+                if(!(passText.getText().toString().equals(repassText.getText().toString()))){
+                    Toast.makeText(getApplicationContext(), "Password fields must match.",
+                            Toast.LENGTH_LONG).show();
+                }
+
+                int ageInt = Integer.parseInt(ageText.getText().toString());
+
+                // checks that age is between 1 and 99.
+                if(ageInt > 99 || ageInt < 1){
+                    Toast.makeText(getApplicationContext(), "Age must be between 1-99.",
+                            Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
+        // button for canceling new user creation and return to login page
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+        cancelButton.setKeyListener(null);
+
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
     }
 }
