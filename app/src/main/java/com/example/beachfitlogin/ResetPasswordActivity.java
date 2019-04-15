dfbe66b0e73e0de2440060f8174dc796232479fe
shockwave1211 <acjgamer@gmail.com>
2019-04-14 22:31:13 -0700

    added non working functionality for password reset CRASHES

---
+++ app/src/main/java/com/example/beachfitlogin/ResetPasswordActivity.java
@@ -0,0 +1,50 @@
+package com.example.beachfitlogin;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    EditText userEmail;
+    Button userPass;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        userEmail = findViewById(R.id.fieldEmail);
+        userPass = findViewById(R.id.forgotPasswordButton);
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        userPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.sendPasswordResetEmail(userEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener <Void>(){
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ResetPasswordActivity.this, "Password sent to your email", Toast.LENGTH_LONG).show();
+                        } else{
+                            Toast.makeText(ResetPasswordActivity.this,
+                                    task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+}
