82195e1aa3f1fd6e757752e9b8bc45e0488a892d
shockwave1211 <acjgamer@gmail.com>
2019-04-14 23:19:51 -0700

    cleaned up the reset password function

--- app/src/main/java/com/example/beachfitlogin/ResetPasswordActivity.java
+++ app/src/main/java/com/example/beachfitlogin/ResetPasswordActivity.java
@@ -1,8 +1,10 @@
 package com.example.beachfitlogin;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,25 +25,41 @@ public class ResetPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
-        userEmail = findViewById(R.id.fieldEmail);
-        userPass = findViewById(R.id.sendResetEmail);
 
+        userEmail = (EditText) findViewById(R.id.fieldEmail);
+        userPass = (Button) findViewById(R.id.sendResetEmail);
         firebaseAuth = firebaseAuth.getInstance();
 
         userPass.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                firebaseAuth.sendPasswordResetEmail(userEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener <Void>(){
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(ResetPasswordActivity.this, "Password reset email has been sent", Toast.LENGTH_LONG).show();
-                        } else{
-                            Toast.makeText(ResetPasswordActivity.this,
-                                    task.getException().getMessage(), Toast.LENGTH_LONG).show();
+            public void onClick(View v)
+            {
+                String Email = userEmail.getText().toString();
+
+                if (TextUtils.isEmpty(Email)) 
+                {
+                    Toast.makeText(ResetPasswordActivity.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    firebaseAuth.sendPasswordResetEmail(Email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(ResetPasswordActivity.this, "Please check your email to reset your password", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                            }
+                            else
+                            {
+                                Toast.makeText(ResetPasswordActivity.this, "error occurred", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+                }
+                
+
             }
         });
 
