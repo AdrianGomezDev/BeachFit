91c32766fdc72bd9185235487a2608465dbbe631
AdrianGomezDev <43425907+AdrianGomezDev@users.noreply.github.com>
2019-04-15 16:29:32 -0700

    i am committed to you :)
    Put in step counter

---
+++ app/src/main/java/com/example/beachfitlogin/StepCounter.java
@@ -0,0 +1,136 @@
+package com.example.beachfitlogin;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class StepCounter extends Fragment implements SensorEventListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    //////////////////////////////////
+
+    private SensorManager mSensorManager;
+    private Sensor mSensorStep;
+    private TextView mCount;
+    boolean activityRunning;
+
+    //////////////////////////////////
+
+    private OnFragmentInteractionListener mListener;
+
+    public StepCounter() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Analytics.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static StepCounter newInstance(String param1, String param2) {
+        StepCounter fragment = new StepCounter();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getActivity().setTitle("Step Counter");
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_analytics, container, false);
+
+        mCount = getActivity().findViewById(R.id.count);
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentMessage("Step Counter", uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            mSensorManager.registerListener(this,countSensor,mSensorManager.SENSOR_DELAY_UI);
+        }
+        else{
+            Toast.makeText(this.getActivity(),"Count sensor not available!",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        activityRunning = false;
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        mCount.setText(String.valueOf(event.values[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
